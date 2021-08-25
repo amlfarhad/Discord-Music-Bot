6d1a1bca1fcddc73bb9016ee8a195b3928bea402
amlfarhad <amal.farhad.shaji@gmail.com>
2021-08-26 00:41:31 +0400

    Created token variable

--- config.py
+++ config.py
@@ -4,9 +4,9 @@ import clairo
 
 cogs = [clairo]
 
-for i in range(len(cogs)):
-    cogs[i].setup()
-
 client = commands.Bot(command_prefix='#', intents = discord.Intents.all())
 
-client.run("ODgwMTM2NTUyMjUxMDI3NDU2.YSZ5Cg.2wncIIGsoQQjNjWyB9KLs1ZrdP0") #this is not the real token
+for i in range(len(cogs)):
+    cogs[i].setup(client)
+
+token = 'ODgwMTM2NTUyMjUxMDI3NDU2.YSZ5Cg.RxxF5qFbYCgaWoNObQTAfjWG1Ss'
