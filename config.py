0f24a68ef554f20a99df65c39bfc3582bc69a28a
amlfarhad <amal.farhad.shaji@gmail.com>
2021-08-26 01:02:33 +0400

    Created environment variable

--- config.py
+++ config.py
@@ -1,6 +1,8 @@
 import discord
 from discord import commands
 import clairo
+from dotenv import load_dotenv
+load_dotenv()
 
 cogs = [clairo]
 
@@ -9,4 +11,3 @@ client = commands.Bot(command_prefix='#', intents = discord.Intents.all())
 for i in range(len(cogs)):
     cogs[i].setup(client)
 
-token = 'ODgwMTM2NTUyMjUxMDI3NDU2.YSZ5Cg.RxxF5qFbYCgaWoNObQTAfjWG1Ss'
