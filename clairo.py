500c52a79ad448298b0b3e1a0d054bbc29f6dfa9
amlfarhad <amal.farhad.shaji@gmail.com>
2021-08-25 21:50:09 +0400

    Second commit

--- clairo.py
+++ clairo.py
@@ -1,1 +1,6 @@
 import discord
+from discord.ext import commands
+
+client = commands.Bot(command_prefix='#', intents = discord.Intents.all())
+
+client.run("ODgwMTM2NTUyMjUxMDI3NDU2.YSZ5Cg.2wncIIGsoQQjNjWyB9KLs1ZrdP0")
