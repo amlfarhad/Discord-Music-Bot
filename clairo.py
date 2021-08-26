b17fb1796f949b8ea6ecc7bbd4c7b614dcd299ce
amlfarhad <amal.farhad.shaji@gmail.com>
2021-08-26 09:48:57 +0400

    Made shorter prefixes for play and disconnect

--- clairo.py
+++ clairo.py
@@ -11,19 +11,54 @@ class music(commands.Cog):
     async def disconnect(self, ctx):
         await ctx.voice_client.disconnect()
 
+    @commands.command()
+    async def d(self, ctx):
+        await ctx.voice_client.disconnect()
 
     @commands.command()
-    async def play(self,ctx,url):
+    async def play(self, ctx, url):
 
         if ctx.author.voice is None:
             await ctx.send("You're not in a voice channel")
         voice_channel = ctx.author.voice.channel
         if ctx.voice_client is None:
             await voice_channel.connect()
+            await ctx.guild.change_voice_state(channel=voice_channel, self_mute=False, self_deaf=True)
         else:
             await ctx.voice_client.move_to(voice_channel)
-        FFMPEG_OPTIONS = {'before_options': '-reconnect 1 -reconnect_streamed 1 -reconnect_delay_max 5','options': '-vn'}
-        YDL_OPTIONS = {'format':"bestaudio"}
+            await ctx.guild.change_voice_state(channel=voice_channel, self_mute=False, self_deaf=True)
+        FFMPEG_OPTIONS = {'before_options': '-reconnect 1 -reconnect_streamed 1 -reconnect_delay_max 5',
+                          'options': '-vn'}
+        YDL_OPTIONS = {'format': "bestaudio"}
+        vc = ctx.voice_client
+
+        with youtube_dl.YoutubeDL(YDL_OPTIONS) as ydl:
+            info = ydl.extract_info(url, download=False)
+            url2 = info['formats'][0]['url']
+            source = await discord.FFmpegOpusAudio.from_probe(url2, **FFMPEG_OPTIONS)
+            vc.play(source)
+
+        with youtube_dl.YoutubeDL(YDL_OPTIONS) as ydl:
+            info = ydl.extract_info(url, download=False)
+            url2 = info['formats'][0]['url']
+            source = await discord.FFmpegOpusAudio.from_probe(url2, **FFMPEG_OPTIONS)
+            vc.play(source)
+
+    @commands.command()
+    async def p(self, ctx, url):
+
+        if ctx.author.voice is None:
+            await ctx.send("You're not in a voice channel")
+        voice_channel = ctx.author.voice.channel
+        if ctx.voice_client is None:
+            await voice_channel.connect()
+            await ctx.guild.change_voice_state(channel=voice_channel, self_mute=False, self_deaf=True)
+        else:
+            await ctx.voice_client.move_to(voice_channel)
+            await ctx.guild.change_voice_state(channel=voice_channel, self_mute=False, self_deaf=True)
+        FFMPEG_OPTIONS = {'before_options': '-reconnect 1 -reconnect_streamed 1 -reconnect_delay_max 5',
+                          'options': '-vn'}
+        YDL_OPTIONS = {'format': "bestaudio"}
         vc = ctx.voice_client
 
         with youtube_dl.YoutubeDL(YDL_OPTIONS) as ydl:
@@ -47,6 +82,7 @@ class music(commands.Cog):
     async def resume(self, ctx):
         await ctx.voice_client.resume()
         await ctx.send("Resumed")
+
 
 def setup(client):
     client.add_cog(music(client))
